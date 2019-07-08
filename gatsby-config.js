b6b97e22e22711860240e889ccd227883ffd3b90
Karol Śliwka <ksliwka@multic.pl>
2019-07-08 15:45:38 +0200

    go go

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,30 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  pathPrefix: `/overview/`,
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "CMS",
+        fieldName: "cms",
+        url: `http://127.0.0.1:8000/api/graphql`,
+      }
+    },
+    /*{
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `overview`,
+        short_name: `overview`,
+        start_url: `/overview`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },*/
+    `gatsby-plugin-offline`,
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,195 @@
-import React from "react"
+import React, { Component } from "react"
+//import df from 'dateformat';
+//import axios from 'axios';
+import { graphql } from "gatsby";
+import Header from '../components/header.js';
+import ExpBox from '../components/expBox.js';
+import BgExp from '../../static/img/bgExp.jpg';
+import BgAbi from '../../static/img/bgAbi.jpg';
+import BgPro from '../../static/img/bgPro.jpg';
+import BgExpBlur from '../../static/img/bgExpBlur.jpg';
+import BgAbiBlur from '../../static/img/bgAbiBlur.jpg';
+import BgProBlur from '../../static/img/bgProBlur.jpg';
+import '../../static/css/animate.css';
+import '../../static/css/blur.css';
 
-export default () => <div>Hello world!</div>
+
+export const query = graphql`{
+  cms {
+    cards {
+      title,
+      link,
+      subtitle,
+      cardType,
+      begin,
+      end,
+      text,
+      img {
+        file
+      },
+      tags {
+        name
+        url
+      }
+      rates {
+        rate,
+        desc,
+        color
+      }
+    }
+  }
+}`
+
+
+class Index extends Component {
+
+  setData() {
+    const { cms } = this.props.data
+    if (cms.cards) {
+      return {
+        exp: cms.cards.filter(item => item.cardType === 'exp'),
+        abi: cms.cards.filter(item => item.cardType === 'abi'),
+        pro: cms.cards.filter(item => item.cardType === 'pro')
+      }
+    }
+  }
+
+  render() {
+    const {exp, abi, pro} = this.setData()
+    return (
+      <div>
+        <Header />
+        <div
+          className={'grid-padding-x'}
+          style={{
+            justifyContent: 'center',
+            padding: '2em',
+            paddingTop: '4em',
+            background: '#000'
+          }}
+        >
+          <h1 style={{ textAlign: 'center', color: 'white', fontWeight: '300' }}>Doświadczenie zawodowe</h1>
+        </div>
+        <div
+          className={'grid-x grid-padding-x small-up-1 medium-up-1 large-up-3'}
+          style={{
+            padding: '2em',
+            justifyContent: 'center',
+            backgroundImage: `url(${BgExp})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'cover'
+          }}
+        >
+          {exp.map(item => {
+            return (
+              <div className={'cell'} key={item.title}>
+                <ExpBox
+                  title={item.title}
+                  img={item.img.file}
+                  subtitle={item.subtitle}
+                  text={item.text}
+                  from={item.begin}
+                  to={item.end}
+                  rates={item.rates}
+                  url={item.link}
+                  tags={item.tags}
+                  imgBlur={BgExpBlur}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className={'grid-padding-x'}
+          style={{
+            justifyContent: 'center',
+            padding: '3em',
+            paddingTop: '4em',
+            background: '#000'
+          }}
+        >
+          <h1 style={{ textAlign: 'center', color: 'white', fontWeight: '300' }}>Projekty</h1>
+        </div>
+        <div
+          className={'grid-x grid-padding-x small-up-1 medium-up-1 large-up-3'}
+          style={{
+            padding: '2em',
+            justifyContent: 'center',
+            backgroundImage: `url(${BgPro})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'cover'
+          }}
+        >
+          {pro.map(item => {
+            return (
+              <div className={'cell'} key={item.title}>
+                <ExpBox
+                  title={item.title}
+                  img={item.img}
+                  subtitle={item.subtitle}
+                  text={item.text}
+                  from={item.from}
+                  to={item.to}
+                  secondRate={item.secondRate}
+                  firstRate={item.firstRate}
+                  url={item.url}
+                  tags={item.tags}
+                  imgBlur={BgProBlur}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className={'grid-padding-x'}
+          style={{
+            justifyContent: 'center',
+            padding: '3em',
+            paddingTop: '4em',
+            background: '#000'
+          }}
+        >
+          <h1 style={{ textAlign: 'center', color: 'white', fontWeight: '300' }}>Umiejętności</h1>
+        </div>
+        <div
+          className={'grid-x grid-padding-x small-up-1 medium-up-1 large-up-3'}
+          style={{
+            padding: '2em',
+            justifyContent: 'center',
+            backgroundImage: `url(${BgAbi})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'cover'
+          }}
+        >
+          {abi.map(item => {
+            return (
+              <div className={'cell'} key={item.title}>
+                <ExpBox
+                  title={item.title}
+                  img={item.img}
+                  subtitle={item.subtitle}
+                  text={item.text}
+                  from={item.from}
+                  to={item.to}
+                  secondRate={item.secondRate}
+                  firstRate={item.firstRate}
+                  url={item.url}
+                  tags={item.tags}
+                  imgBlur={BgAbiBlur}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
